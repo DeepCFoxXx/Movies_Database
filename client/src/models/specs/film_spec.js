30d8874f1779f0450f6be45268886005e80c70bd
DeepCFoxXx <learncode23@gmail.com>
2018-04-18 15:06:39 +0100

    movie has title passing

---
+++ client/src/models/specs/film_spec.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var Film = require('../film.js');
+
+describe('Film', function() {
+  var film;
+
+  beforeEach(function() {
+    film = new Film({
+      title: 'Aliens',
+      genres: ['Sci Fi']
+    });
+  });
+
+  it('should have title', function() {
+    assert.equal(film.title, 'Aliens');
+  });
+
+});
