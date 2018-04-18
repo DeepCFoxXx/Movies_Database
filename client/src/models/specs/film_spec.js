4c12a59ae15c3c65781dfd5542b7db7175f17877
DeepCFoxXx <learncode23@gmail.com>
2018-04-18 15:09:48 +0100

    movie has actors passing

--- client/src/models/specs/film_spec.js
+++ client/src/models/specs/film_spec.js
@@ -7,12 +7,17 @@ describe('Film', function() {
   beforeEach(function() {
     film = new Film({
       title: 'Aliens',
+      actors: 'Sigourney Weaver',
       genres: ['Sci Fi']
     });
   });
 
   it('should have title', function() {
     assert.equal(film.title, 'Aliens');
+  });
+
+  it('should have actors', function() {
+    assert.equal(film.actors, 'Sigourney Weaver');
   });
 
 });
