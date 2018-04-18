e8057f14c55ada4ef1b7f9b433cb84eb5fc8475d
DeepCFoxXx <learncode23@gmail.com>
2018-04-18 15:52:57 +0100

    get request added to films function

--- client/src/models/films.js
+++ client/src/models/films.js
@@ -10,6 +10,16 @@ Films.prototype = {
     request.setRequestHeader("Content-type", "application/json");
     request.onload = callback;
     request.send(payload);
+  },
+  all: function(callback){
+    var self = this;
+    this.makeRequest("GET", "http://localhost:3000/api/films", function(){
+      if(this.status !== 200) return;
+      var jsonString = this.responseText;
+      var results = JSON.parse(jsonString);
+      var films = self.populateFilms(results);
+      callback(films);
+    });
   }
 };
 
