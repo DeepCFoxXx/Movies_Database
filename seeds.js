e465f604d5ae28f21bd8b0ed38934de277555248
DeepCFoxXx <learncode23@gmail.com>
2018-04-19 14:34:55 +0100

    seeds added

--- seeds.js
+++ seeds.js
@@ -1,1 +1,23 @@
 use movies;
+
+db.films.insert([
+  {
+    title: "Army of Darkness",
+    actors: ["Bruce Campbell", "Embeth Davidtz"],
+    genre: "Fantasy/Horror",
+    reviews: [{
+      comment: "Near perfect use of skeletons.",
+      rating: 7,
+      author: "Kara"}]
+  },
+  {
+    title: "Dredd",
+    actors: ["Karl Urban", "Olivia Jo Thirlby"],
+    genre: "Action/Sci fi",
+    reviews: [{
+      comment: "Its the raid with Judge Dredd so its fantastic",
+      rating: 8,
+      author: "Kara"
+    }]
+  }]
+);
